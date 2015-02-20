1d56a574764723531eb53053b785fe23342d3077
coilo <coilo.dev@gmail.com>
2015-02-20 12:31:25 +0900

    Add badge to README.md

--- project.clj
+++ project.clj
@@ -1,11 +1,14 @@
 (defproject profile "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Coilo central hub for open source"
+  :url "https://github.com/coilo/profile"
   :min-lein-version "2.0.0"
+  :license {:name "The MIT License (MIT)"
+            :url "http://opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
                  [ring/ring-defaults "0.1.2"]]
-  :plugins [[lein-ring "0.8.13"]]
+  :plugins [[lein-ring "0.8.13"]
+            [lein-cloverage "1.0.2"]]
   :ring {:handler profile.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
