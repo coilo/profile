bb07c2dbaaaf705a4a0590636fae4158f832c115
coilo <coilo.dev@gmail.com>
2015-03-01 01:59:58 +0900

    Add package "enlive"

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
             :url "http://opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
+                 [enlive "1.1.5"]
                  [ring/ring-defaults "0.1.2"]]
   :plugins [[lein-ring "0.8.13"]
             [lein-cloverage "1.0.2"]]
